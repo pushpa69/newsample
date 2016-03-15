5c55fd1f30a37fc71472441b6b8682e66c11572a
pushpa10 <pushpa.edu53@gmail.com>
2016-03-14 23:12:26 -0700

    validations

--- index.php
+++ index.php
@@ -12,14 +12,14 @@
 					<h2>School of computer science and engineering</h2>
 				</div>
 				<div id="login">
-					<form>
+					<form action="" method="POST" onsubmit="return validate();" name="myform">
 						<div>
 							<label>User Name:</label>
-							<input type="text" id="user_id" name="user_id" value=""><br><br>
+							<input type="text" id="user_id" name="user_id" value=""  onblur="myfunction()"/><br><br>
 						</div>
 						<div>
 							<label>Password:</label>
-							<input type="password" id="pwd" name="pwd" value=""><br><br>
+							<input type="password" id="pwd" name="pwd" value=""  onblur="myfunction()"/><br><br>
 						</div>
 						<div style="margin:17px 0px 0px 166px;">
 							<input type="submit" id="button" onclick="" value="sign in">
@@ -30,7 +30,35 @@
 			<div style="text-align:right;margin-right:20px;">
 			<a href="ParticpantInfo.php"><button>Next</button></a>
 			</div>	
-				<h1>hello</h1>
+		<script>
+			function validate()
+			{
+				//alert("ok");
+				var fname=document.myform.user_id.value;
+				var pass=document.myform.pwd.value;
+				var fexp=/^[a-z0-9._]+@+[a-z0-9]+.+[a-z]{2,4}/;
+				var epsd=/^[0-9a-zA-Z]+$/;
+				if(!fname.match(fexp))
+				{                             
+					document.getElementById("user_id").style.border="1px solid red";
+					document.myform.user_id.focus();
+					return false;
+				}
+				if(!pass.match(epsd))
+				{                             
+					document.getElementById("pwd").style.border="1px solid red";
+					document.myform.pwd.focus();
+					return false;
+				}
+				
+				
+			}
+			function myfunction()
+				{
+					document.getElementById("user_id").style.border="";
+					document.getElementById("pwd").style.border="";
+				}
+		</script>
 		</body>
 		<?php include("footer.php");?>	 
 </html>
