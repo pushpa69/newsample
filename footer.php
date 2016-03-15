9f4dfa59c33446e3f3df0723f787414f50351b93
Vijay Raghava Reddy <gvijayaraghavareddy7@gmail.com>
2016-03-15 18:19:19 +0530

    footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 <div id="footer" class="footer">
-	<p>Copyright @2015	<a href="http://aapthitech.com">Aapthi Technologies,</a>an SBU of Yalavarthi Software Solutions pvt Ltd.</p>
+	<p>Copyright @2016	<a href="http://aapthitech.com">Aapthi Technologies,</a>an SBU of Yalavarthi Software Solutions pvt Ltd.</p>
 </div>
 </body>
 </html>
