18a3257c563a46eedeada1456fed2db2ca6c7cee
Vijay Raghava Reddy <gvijayaraghavareddy7@gmail.com>
2016-03-15 13:14:39 +0530

    wrapper,container added

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/mystyle.css"></link>
-		<link rel="stylesheet" type="text/css" href="css/reset.css"></link>
-	</head>	
-			<div id="footer">
-				<p>Copyright @2015	<a href="http://aapthitech.com">Aapthi Technologies,</a>an SBU of Yalavarthi Software Solutions pvt Ltd.</p>
-			</div>
+
+<div id="footer" class="footer">
+	<p>Copyright @2015	<a href="http://aapthitech.com">Aapthi Technologies,</a>an SBU of Yalavarthi Software Solutions pvt Ltd.</p>
+</div>
+</body>
 </html>
