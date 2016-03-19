58efe65b43c0cecf4f3b6a87a97e6dc955f1c02c
Vijay Raghava Reddy <gvijayaraghavareddy7@gmail.com>
2016-03-19 08:03:39 +0530

    login.php add

---
+++ admin-1/login.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div id="login">
+			<form action="" method="POST" onsubmit="return validate();" name="myform">
+				<div>
+					<label>User Name:</label>
+					<input type="text" id="user_id" name="user_id" value=""  onblur="myfunction()"/><br><br>
+				</div>
+				<div>
+					<label>Password:</label>
+					<input type="password" id="pwd" name="pwd" value=""  onblur="myfunction()"/><br><br>
+				</div>
+				<div style="margin:17px 0px 0px 166px;" class="btn">
+					<input type="submit" id="button" name="submit" onclick="" value="login"/>
+				</div>
+			</form>
+	</body>
+</html>
