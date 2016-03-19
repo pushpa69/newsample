612103dbef04d85cff6fd320f1ec4eb0a93a87e3
Vijay Raghava Reddy <gvijayaraghavareddy7@gmail.com>
2016-03-19 10:34:21 +0530

    login sucessfully

--- admin-1/index.php
+++ admin-1/index.php
@@ -1,42 +1,54 @@
-<html>
-<head>
-		<link rel="stylesheet" type="text/css" href="loginstyle.css"></link>
-</head>
-<?php include('menu.php');?>
-<body style="background-color:FloralWhite">
-	<div class="wrapper">		
-		<div class="container">
-			<div id="page1">
-				<div id="pagelogo">
-					<img src="trunk/images/logo.png"/>
-					<h2>School of computer science and engineering</h2>
-				</div>
+<?php
+	mysql_connect('localhost','root','');
+	mysql_select_db('salford');		
+		if(isset($_POST['submit'])){	
+		$sql=mysql_query("SELECT * FROM adminlogin WHERE email='".$_POST['email']."' AND password='".$_POST['password']."'");
+		$rows = mysql_num_rows($sql);
+		$result=mysql_fetch_array($sql);
+		if ($rows == 1){
+			echo "login successful";
 			
-				<div id="login">
-					<form action="" method="POST" onsubmit="return validate();" name="myform">
-						<div>
-							<label>User Name:</label>
-							<input type="text" id="user_id" name="user_id" value=""  onblur="myfunction()"/><br><br>
-						</div>
-						<div>
-							<label>Password:</label>
-							<input type="password" id="pwd" name="pwd" value=""  onblur="myfunction()"/><br><br>
-						</div>
-						<div style="margin:17px 0px 0px 166px;" class="btn">
-							<input type="submit" id="button" name="submit"
-							onclick="" value="sign in"/>
-						</div>
-					</form>
-				</div><br>
-				
-				  <!--<div>
-					 <a href="ParticpantInfo.php" 
-					 style="float:right;margin-right:20px;"><button>Next</button></a>
-				 </div> -->
+			header ('location: user.php');
+		}else{
+			echo "<script>";
+			echo " alert('you don't have account with this email.');</script>";
+			header ('location: index.php');
+		}
+	}
+?>
+<html>	
+	<head>
+		<link rel="stylesheet" type="text/css" href="user style.css"></link>
+	</head>	
+	<body>
+			<div id="logo1">
+				<img src="logo.png" alt="logo" style="width:150px;height:90px;">
+			</div>
+			<div id ="menu1">
+				<h1 style="text-align:center;">Cloud Framework Tool</h1>
 			</div>			
-		</div>	
-		<div class="push"></div>			
-	</div>	<br>
-	
-	
+			<div id="menu">
+				<ul>				  
+				  <li><a href="" style="margin-left:50px; font-size:20px;color:white;"> Login</a></li>				  
+				</ul>				
+			</div>
+			<div id="login">
+				<form action="" method="POST">
+					<div>
+						<label>Email</label>
+						<input type="email" name="email" id="email" value="">
+					</div>
+					<div>
+						<label>Password</label>
+						<input type="password" name="password" id="password" value="">
+					</div>
+					<div>
+						<input style="margin-left:300px;"type="submit" name="submit" value="login"/>
+					</div>
+				</form>
+			</div>
+	</body>	
+		<footer>  
+			<p style="text-align:center;">Copyright @2016	<a href="http://aapthitech.com">Aapthi Technologies,</a>an SBU of Yalavarthi Software Solutions pvt Ltd.</p>
+		</footer>
 </html>
