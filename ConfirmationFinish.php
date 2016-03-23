e265265fcd027a519fbf322e6857adc571356eb2
Vijay Raghava Reddy <gvijayaraghavareddy7@gmail.com>
2016-03-23 11:03:59 +0530

    crs

--- ConfirmationFinish.php
+++ ConfirmationFinish.php
@@ -142,6 +142,26 @@
 					<p>1.Cloud Requirement Stage:</p><br>					
 				</div>
 				<div>
+					<?php $result=mysql_query('SELECT * FROM general_information WHERE gi_u_id="'.$_GET['userid'].'"');
+					 // echo'<pre>'; print_r($result);exit;
+					 $arr=mysql_fetch_array($result);
+					 // echo'<pre>';
+					 // print_r($arr);
+					 // exit;
+					 if ($arr['size_enterprise']="micro"){ 
+					 echo "Services suitable are IaaS** or SaaS* on public cloud depending upon usage and requirement.";
+					 }else if($arr['size_enterprise']="small"){
+						 echo "Service suitable are IaaS as a standalone service on public cloud, else amalgamated services (IaaS and PaaS***) on hybrid cloud depending upon usage and requirement. ";
+					 }else if($arr['size_enterprise']="medium"){
+						  echo "Service suitable are either SaaS as a standalone service on public cloud or amalgamated services (IaaS and PaaS) on hybrid cloud or two or more services (PaaS, IaaS, SaaS) on hybrid, depending upon usage and requirement. ";
+					 }echo "NOTE:
+							*SaaS(software as a service): could be an email solution or any software solution like customer relationship management (CRM), content management system (CMS), Enterprise resource planning (ERP), HR related software’s etc.
+							**IaaS(Infrastructure as a service): could be a remote data center for storing and managing data, virtual monitors (machines), cloud hosting and networks, etc. 
+							***Paas(platform as a Service): could be a platform for development like operating systems, virtualized servers, various frameworks used for development etc.
+							";					 
+					?>
+				</div>
+				<div>
 					<p>2.Cloud Preparation Stage:</p><br>
 					<p style="color:green;font-size:20px;font-weight:bold;">>Your Preparedness</p><br>
 					<div>
